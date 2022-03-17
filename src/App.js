92da485354b1ff620d7dda0d8e23d8e47dfe95cc
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-03-17 20:18:40 +0700

    update setup project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <h1 className='text-5xl'>Hello World</h1>
     </div>
   );
 }
