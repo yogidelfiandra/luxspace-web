d6fe0a98ea13dd9856225b465e20b9c1271b25d0
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-03-21 21:35:20 +0700

    create footer section

---
+++ src/parts/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer class='flex text-center px-4 py-8 justify-center'>
+      <p class='text-sm'>
+        Copyright 2022 • All Rights Reserved LuxSpace by{' '}
+        <a
+          href='https://www.instagram.com/yogidelfiandra/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          yogidelfiandra
+        </a>
+      </p>
+    </footer>
+  );
+}
