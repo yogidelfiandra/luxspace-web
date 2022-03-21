6d4365879ff19f6a779348204e0aaac296c37478
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-03-21 20:21:16 +0700

    create JustArrived section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
+import JustArrived from 'parts/HomePage/JustArrived';
 import React from 'react';
 import Header from '../parts/Header';
 import Hero from '../parts/Hero';
+import BrowseRoom from '../parts/HomePage/BrowseRoom';
 
 export default function HomePage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
+      <JustArrived />
     </>
   );
 }
