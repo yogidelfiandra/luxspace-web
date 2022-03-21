4a64f7980ed3478a67887dde7b484646ca0feb3c
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-03-21 21:34:45 +0700

    create clients section

---
+++ src/parts/Clients.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Clients() {
+  return (
+    <section
+      class='container mx-auto'
+      style={{ paddingTop: 36, paddingBottom: 36 }}
+    >
+      <div class='flex justify-center flex-wrap'>
+        <div class='w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0'>
+          <img src='./images/content/logo-adobe.svg' alt='' class='mx-auto' />
+        </div>
+        <div class='w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0'>
+          <img src='./images/content/logo-ikea.svg' alt='' class='mx-auto' />
+        </div>
+        <div class='w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0'>
+          <img
+            src='./images/content/logo-hermanmiller.svg'
+            alt=''
+            class='mx-auto'
+          />
+        </div>
+        <div class='w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0'>
+          <img src='./images/content/logo-miele.svg' alt='' class='mx-auto' />
+        </div>
+      </div>
+    </section>
+  );
+}
