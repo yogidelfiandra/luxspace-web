72d368d86825637f7c562d9cafc9206b6989aa10
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-03-24 15:05:19 +0700

    add useScrollAnchor and useModalDOM

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,5 @@
+import useModalDOM from 'helpers/hooks/useModalDOM';
+import useScrollAnchor from 'helpers/hooks/useScrollAnchor';
 import Clients from 'parts/Clients';
 import JustArrived from 'parts/HomePage/JustArrived';
 import Sitemap from 'parts/Sitemap';
@@ -7,7 +9,10 @@ import Header from '../parts/Header';
 import BrowseRoom from '../parts/HomePage/BrowseRoom';
 import Hero from '../parts/HomePage/Hero';
 
-export default function HomePage(props) {
+export default function HomePage() {
+  useScrollAnchor();
+  useModalDOM();
+
   return (
     <>
       <Header theme='white' position='absolute' />
